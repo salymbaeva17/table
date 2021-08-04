d9a6a3f62162c537c044c479e8711e463fb9f8ed
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-04 09:31:11 +0600

    second option

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const divs = document.querySelectorAll(".wrapper div")
 const colors = ["yellow", "green", "blue", "purple", "red", "aquamarine"]
+const wrapper = document.querySelector(".wrapper")
 const text = ["Тут вам не здесь", "Тут вам не там", "Мы это не вы", "Их это не наш", "Я это не ты", "Наш значит не ваш"]
 
-divs.forEach((item) =>
-    item.addEventListener("click", () => {
-        divs.forEach(el => el.removeAttribute("class"))
-        const randomNumber = Math.floor(Math.random() * 6)
-        item.classList.add(colors[randomNumber])
-        item.innerText = text[randomNumber]
-    })
-)
+
+// ------------ FIRST OPTION ----------- работает медленнее
+// divs.forEach((item) =>
+//     item.addEventListener("click", () => {
+//         divs.forEach(el => el.removeAttribute("class"))
+//         const randomNumber = Math.floor(Math.random() * 6)
+//         item.classList.add(colors[randomNumber])
+//         item.innerText = text[randomNumber]
+//     })
+// )
+
+
+// ------------- SECOND OPTION ----------
+wrapper.addEventListener("click", (e) => {
+    divs.forEach(el => el.removeAttribute("class"))
+    const randomNumber = Math.floor(Math.random() * 6)
+    e.target.classList.add(colors[randomNumber])
+    e.target.innerText = text[randomNumber]
+})
 
 
